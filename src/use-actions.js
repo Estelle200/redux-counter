e36ebd4ce1b72a4a36aad90d9139256cccb0a6bb
Ojinji Esther <46982609+Estelle200@users.noreply.github.com>
2024-07-16 13:46:23 +0000

    "use-counter"

--- src/use-actions.js
+++ src/use-actions.js
@@ -1,7 +1,11 @@
 import { bindActionCreators } from 'redux';
 import { useDispatch } from 'react-redux';
+import { useMemo } from 'react';
 
 export const useActions = (actions) => {
   const dispatch = useDispatch();
-  return bindActionCreators(actions, dispatch);
+  return useMemo(
+    () => bindActionCreators(actions, dispatch),
+    [actions, dispatch]
+  );
 };
